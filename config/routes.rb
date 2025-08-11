292f6b14baaef5e78d49a7f604b83062c4ad8a2d
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2025-08-12 02:29:34 +0800

    feat(routes): nest clients resource under users in API v1 utes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users do
+        resources :clients
+      end
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
