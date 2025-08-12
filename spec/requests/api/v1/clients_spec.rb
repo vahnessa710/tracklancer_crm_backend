c2bf0e010f2a404a16e3ccacf5c4e9bcbddb91b1
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2025-08-13 00:26:54 +0800

    test(api): add request specs for Clients#show and Clients#update endpoints

--- spec/requests/api/v1/clients_spec.rb
+++ spec/requests/api/v1/clients_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Clients API", type: :request do
   let!(:user)    { create(:user) }
   let!(:clients) { create_list(:client, 2, user: user) }
+  let(:client) { clients.first }
 
   describe "GET /api/v1/users/:user_id/clients" do
     it "returns all clients for a user" do
@@ -25,4 +26,28 @@ RSpec.describe "Clients API", type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+   describe "GET /api/v1/users/:user_id/clients/:id" do
+    it "returns a specific client" do
+      get api_v1_user_client_path(user_id: user.id, id: client.id)
+
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json["id"]).to eq(client.id)
+      expect(json["name"]).to eq(client.name)
+    end
+  end
+
+  describe "PATCH /api/v1/users/:user_id/clients/:id" do
+    it "updates an existing client" do
+      update_params = { client: { name: "Updated Client Name" } }
+
+      patch api_v1_user_client_path(user_id: user.id, id: client.id), params: update_params
+
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json["data"]["name"]).to eq("Updated Client Name")
+    end
+  end
+
 end
